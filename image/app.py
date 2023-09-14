62f0a7c4b6ec0abf60528baa902c3b61b38c0edf
Violin Yanev <violinyanev@users.noreply.github.com>
2023-09-14 17:13:27 +0200

    Add a docker compose wrapper

--- image/app.py
+++ image/app.py
@@ -4,9 +4,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/health', methods=['GET'])
-def health_check():
-    return "API is healthy", 200
+@app.route('/health')
+def health():
+    return 'OK', 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
