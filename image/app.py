121c12ccf96d03c1d1dc2838c4da772cfe6f32e2
Violin Yanev <violinyanev@users.noreply.github.com>
2023-11-12 11:48:12 +0100

    Implement file upload endpoint

--- image/app.py
+++ image/app.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 
-from flask import Flask
+from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
 from pathlib import Path
+import sys
 
 app = Flask(__name__)
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/health', methods=['GET'])
@@ -11,9 +19,30 @@ def health():
     return 'OK', 200
 
 
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
+
+    file = request.files['file']
+
+    # If the user does not select a file, the browser submits an empty file without a filename
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        filepath = Path(app.config['UPLOAD_FOLDER']) / filename
+        file.save(filepath)
+        return jsonify({'message': 'File uploaded successfully', 'filename': filename})
+    else:
+        return jsonify({'error': 'Invalid file format'})
+
+
 @app.route('/albums', methods=['GET'])
 def albums():
-    directory = Path('/var/data') # TODO make configurable
+    # Set the upload folder
 
     if directory.is_dir():
         albums = {
@@ -38,4 +67,11 @@ def albums():
 
 
 if __name__ == '__main__':
+    app.config['UPLOAD_FOLDER'] = '/var/data'
+    if len(sys.argv) == 2:
+        app.config['UPLOAD_FOLDER'] = sys.argv[1]
+
+    folder = Path(app.config['UPLOAD_FOLDER'])
+    if not folder.exists():
+        folder.mkdir(parents=True, exist_ok=True)
     app.run(host='0.0.0.0', port=5000)
