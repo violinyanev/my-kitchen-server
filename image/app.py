9b812c0a4f717d64e33a3dcc7ae2e1c328da784f
Violin Yanev <violinyanev@users.noreply.github.com>
2024-01-12 13:49:48 +0100

    Fix log

--- image/app.py
+++ image/app.py
@@ -112,8 +112,7 @@ def delete_recipe(recipe_id):
 
 
 if __name__ == '__main__':
-    v=get_api_version()
-    print(f"API version: {v[0]}.{v[1]}.{v[2]}")
+    print(f"API version: {get_api_version()}")
 
     app.config['UPLOAD_FOLDER'] = '/tmp/data'
 
